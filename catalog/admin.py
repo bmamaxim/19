c589d96742b9be1ab3e8b17855565ea6566d73e0
bmamaxim <bma@gmail.com>
2024-03-03 13:08:37 +0500

    admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from catalog.models import Categories, Products
+
+
 # Register your models here.
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name',)
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_name', 'product_price', 'product_category',)
+    list_filter = ('product_category',)
+    search_fields = ('product_name', 'product_description',)
--- catalog/models.py
+++ catalog/models.py
@@ -6,7 +6,6 @@ from django.db import models
 NULLABLE = {'blank': True, 'null': True}
 
 
-
 class Categories(models.Model):
     category_name = models.CharField(max_length=200, verbose_name='категория')
     category_description = models.CharField(max_length=200, verbose_name='описание')
@@ -18,21 +17,23 @@ class Categories(models.Model):
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
 
+
 class Products(models.Model):
     product_name = models.CharField(max_length=200, verbose_name='название')
     product_description = models.CharField(max_length=200, verbose_name='описание', **NULLABLE)
     product_image = models.ImageField(upload_to='image/', verbose_name='изображение', **NULLABLE)
     product_category = models.ForeignKey(Categories, on_delete=models.CASCADE, verbose_name='категория')
     product_price = models.IntegerField(verbose_name='цена', **NULLABLE)
-    manufactured_at = models.DateField(verbose_name='дата производства', **NULLABLE)
     created_at = models.DateField(auto_now_add=True, verbose_name='дата записи', **NULLABLE)
     updated_at = models.DateField(auto_now_add=True, verbose_name='последние изменения', **NULLABLE)
 
-
     def __str__(self):
-        return f'{self.product_name}'
+        return (
+            f'{self.product_name}'
+            f'{self.product_price}'
+            f'{self.product_category}'
+        )
 
     class Meta:
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
-
