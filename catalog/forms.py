3667d9184354eeb48eb66ad142d156afc24f716c
bmamaxim <bma@gmail.com>
2024-04-28 17:55:07 +0500

    perm

--- catalog/forms.py
+++ catalog/forms.py
@@ -11,7 +11,7 @@ class StyleFormMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for fild_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['class'] = ''
 
 
 class ProductsForm(StyleFormMixin,
